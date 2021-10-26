6938d3e240eacb8860c8645073f0b45a785f16bc
Selim GOMRI <selimgomri@gmail.comssh git github.comssh git@github.comcdssh git@github.comss-keygen -t rsagit config --global user.email selimgomri@gmail.com>
2021-10-26 17:46:57 +0200

    form created and connected with database'

---
+++ create.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>create</title>
+</head>
+
+<body>
+
+    <header>
+        <nav>
+            <ul>
+                <li><a href='http://localhost:8000/create.php'>create</a></li>
+                <li><a href='http://localhost:8000'>read</a></li>
+                <li><a href='http://localhost:8000/aux_create.php'>create_aux</a></li>
+
+            </ul>
+        </nav>
+    </header>
+    <!--siging database -------------------------------------------------------------------------------------->
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "library";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT id, name FROM author";
+    $result = $conn->query($sql);
+
+    ?>
+
+    <!-- form to get the data -->
+    <form action="aux_create.php" method="POST">
+        <h1> Add book </h1>
+        <label for="title">Enter book title: </label>
+        <input type="text" name='title' id="title">
+        <br />
+
+        <label for="release_date">Enter release date: </label>
+        <input type="integer" name='release_date' id="release_date">
+        <br />
+
+        <label for="author_id">Choose an author: </label>
+        <select name="author_id" id ="author_id">
+            <option value="">...</option>
+            <?php
+            // author menu
+            while ($row = $result->fetch_assoc()) {
+            ?> <option value=<?php echo $row["id"] ?>> <?php echo $row["name"] ?> </option>
+            <?php } ?>
+
+        </select>
+        <br />
+
+        <input type="submit" value="submit">
+
+    </form>
+
+
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -9,6 +9,16 @@
 </head>
 
 <body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href='http://localhost:8000/create.php'>create</a></li>
+                <li><a href='http://localhost:8000'>read</a></li>
+                <li><a href='http://localhost:8000/aux_create.php'>create_aux</a></li>
+
+            </ul>
+        </nav>
+    </header>
     <?php
     $servername = "localhost";
     $username = "root";
@@ -22,7 +32,7 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "SELECT title, release_date, name FROM book JOIN author ON book.author_id=author.id";
+    $sql = "SELECT title, release_date, name, readers_note FROM book JOIN author ON book.author_id=author.id";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
@@ -32,13 +42,18 @@
                 <th>Title</th>
                 <th>Release Date</th>
                 <th>Author</th>
+                <th>Readers_note</th>
+
             </tr>
-            <?php // output data of each row ?>
+            <?php // output data of each row 
+            ?>
             <?php while ($row = $result->fetch_assoc()) {
             ?> <tr>
                     <td> <?php echo $row["title"]; ?></td>
                     <td><?php echo $row["release_date"]; ?> </td>
                     <td> <?php echo $row["name"]; ?> </td>
+                    <td> <?php echo $row["readers_note"]; ?> </td>
+
                 </tr>
             <?php } ?>
         </table>
