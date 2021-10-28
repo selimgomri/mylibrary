25586bbb96988283a727c3fa3bf95ebf1fb287d9
Selim GOMRI <selimgomri@gmail.comssh git github.comssh git@github.comcdssh git@github.comss-keygen -t rsagit config --global user.email selimgomri@gmail.com>
2021-10-28 10:51:42 +0200

    aux_create.php deleted request messages colored bg and body styled

--- create.php
+++ create.php
@@ -7,12 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>create</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
 </head>
 
 <body>
     <header>
         <nav>
-            <button><a href='http://localhost:8000'>Home</a></button>
+            <h1><a href='http://localhost:8000'>Mylibrary</a></h1>
         </nav>
     </header>
     <!--siging database -------------------------------------------------------------------------------------->
@@ -25,14 +27,14 @@
     ?>
 
     <!-- form to get the data -->
-    <form action="aux_create.php" method="POST">
-        <h1> Add book </h1>
+    <form method="POST">
+        <h2> Add book </h2>
         <label for="title">Enter book title: </label>
-        <input type="text" name='title' id="title">
+        <input type="text" name='title' id="title" required>
         <br />
 
-        <label for="release_date">Enter release date: </label>
-        <input type="text" name='release_date' id="release_date">
+        <label for="release_year">Enter release date: </label>
+        <input type="text" name='release_year' id="release_year" required>
         <br />
 
         <label for="author_id">Choose an author: </label>
@@ -41,17 +43,43 @@
             <?php
             // author menu
             while ($row = $result->fetch_assoc()) {
-            ?> <option value=<?php echo $row["id"] ?>> <?php echo $row["name"] ?> </option>
+            ?> <option value=<?php echo $row["id"] ?> required> <?php echo $row["name"] ?> </option>
             <?php } ?>
-
 
         </select>
         <br />
 
-
         <input type="submit" value="submit">
 
     </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        // collect value of input field
+        $data = $_REQUEST;
+
+        if (empty($data['title']) || empty($data['release_year']) || empty($data['author_id'])) {
+            ?> <p class="request_error"> <?php echo "data is empty";?> </p> <?php
+        } else {
+
+            $sql = "INSERT INTO book (title, release_year, author_id) VALUES ('" . mysqli_real_escape_string($conn, $data['title']) . "', " . $data['release_year'] . ", " . $data['author_id'] . ")";
+
+
+
+            if ($conn->query($sql) === TRUE) {
+                ?> <p class="request_done"> <?php echo "New book added successfully"; ?> 
+                </p>
+                <?php // header("Location: index.php"); // redirects to read page
+            } 
+            else {
+            ?> <p class="request_error"> <?php echo "Error: " . $sql . "<br>" . $conn->error; ?> </p>
+            <?php
+            }
+        }
+    }
+    $conn->close();
+    ?>
 
 
 
--- edit.php
+++ edit.php
@@ -7,13 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
-
     <header>
         <nav>
-            <button><a href='http://localhost:8000'>Home</a></button>
+            <h1><a href='http://localhost:8000'>Mylibrary</a></h1>
         </nav>
     </header>
     <!--siging database -------------------------------------------------------------------------------------->
@@ -23,7 +23,7 @@
 
     $id = $_GET['id']; // get id through query string
 
-    $sql = "SELECT title, release_date, author_id, name from book JOIN author ON author_id=author.id WHERE book.id='$id'"; // select query
+    $sql = "SELECT title, release_year, author_id, name from book JOIN author ON author_id=author.id WHERE book.id='$id'"; // select query
     $result = $conn->query($sql); // select query
 
 
@@ -33,11 +33,11 @@
     if (isset($_POST['update'])) // when click on Update button
     {
         $title = mysqli_real_escape_string($conn, $_POST['title']);
-        $releaseDate = $_POST['release_date'];
+        $releaseYear = $_POST['release_year'];
         $authorId = $_POST['author_id'];
 
 
-        $edit = mysqli_query($conn, "UPDATE book SET title='$title', release_date='$releaseDate', author_id='$authorId' WHERE book.id='$id'");
+        $edit = mysqli_query($conn, "UPDATE book SET title='$title', release_year='$releaseYear', author_id='$authorId' WHERE book.id='$id'");
 
         echo "Book successfully updated";
 
@@ -47,7 +47,6 @@
             exit;
         } else {
             echo mysqli_error($conn);
-            
         }
     }
     ?>
@@ -58,8 +57,8 @@
         <input type="text" name='title' value="<?php echo $data['title'] ?>">
         <br />
 
-        <label for="release_date">Edit release date: </label>
-        <input type="text" name='release_date' value="<?php echo $data['release_date'] ?>">
+        <label for="release_year">Edit release date: </label>
+        <input type="text" name='release_year' value="<?php echo $data['release_year'] ?>">
         <br />
 
         <?php // authors for the menu selection
