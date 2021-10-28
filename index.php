eca07f2f6d51e51986863a35563d62aa8718709e
Selim GOMRI <selimgomri@gmail.comssh git github.comssh git@github.comcdssh git@github.comss-keygen -t rsagit config --global user.email selimgomri@gmail.com>
2021-10-28 14:40:00 +0200

    since year filter created

--- index.php
+++ index.php
@@ -15,16 +15,19 @@
     <header>
         <h1>Mylibrary</h1>
         <nav>
-            <form method="POST" class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">          
+            <form method="POST" class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
                 <input type="search" name="search" class="form-control" placeholder="Search...">
                 <button>search</button>
-                <select>
+                <br />
+                Book released after year
+                <select name="yearFilter">
                     <option value="">..</option>
-                    <?php 
-                    for ($i=1900;$i<2021;$i+=10) { ?>
-                        <option value=""><?php echo $i; ?> </option>
+                    <?php
+                    for ($i = 1900; $i < 2021; $i += 10) { ?>
+                        <option value="<?php echo $i ?>"><?php echo $i; ?> </option>
                     <?php } ?>
                 </select>
+                included
             </form>
             <button><a href='create.php'>Add book</a></button>
         </nav>
@@ -35,29 +38,38 @@
 
     $sql = "SELECT book.id, title, release_year, name FROM book JOIN author ON book.author_id=author.id ";
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_REQUEST['search'])) {
-        
-        $data = explode(" ", preg_replace('/\s+/', ' ', $_REQUEST['search']));
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (!empty($_REQUEST['search'])) {
+
+            $data = explode(" ", preg_replace('/\s+/', ' ', $_REQUEST['search']));
 
 
-        if (!empty($data)) {
-            echo "Results for '" . $_REQUEST['search'] . "'";
-            $conditions = [];
+            if (!empty($data)) {
+                echo "Results for '" . $_REQUEST['search'] . "' ";
+                $conditions = [];
 
-            for ($i = 0; $i < count($data); $i++) {
-                $conditions[] = "title LIKE '%" . $data[$i] . "%'";
-                $conditions[] = "release_year LIKE '%" . $data[$i] . "%'";
-                $conditions[] = "name LIKE '%" . $data[$i] . "%'";
+                for ($i = 0; $i < count($data); $i++) {
+                    $conditions[] = "title LIKE '%" . $data[$i] . "%'";
+                    $conditions[] = "release_year LIKE '%" . $data[$i] . "%'";
+                    $conditions[] = "name LIKE '%" . $data[$i] . "%'";
+                }
+
+                if (!empty($conditions)) {
+
+                    $sql .= "WHERE (" . implode(" OR ", $conditions) . ")";
+                }
             }
+        }
+        if (!empty($_REQUEST['yearFilter'])) {
 
-            if (!empty($conditions)) {
-                
-                $sql .= "WHERE " . implode(" OR ", $conditions);
-            }
+            $sql .= " AND release_year >= " . $_REQUEST['yearFilter'];
+            echo "Since year '" . $_REQUEST['yearFilter'] . "'";
         }
     }
 
-    $sql .= "ORDER BY title ASC";
+    $sql .= " ORDER BY title ASC";
+
+
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
